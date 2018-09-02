170d5b79fcc65d9999ca6d6334ea93081c8ea056
Tobiasz18 <tobiaszpost@gmail.com>
2018-09-02 12:52:15 +0200

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,17 @@ module.exports = {
             {
                 test:/\.js$/,
                 loader: "babel-loader"
-            }
+            },
+                test:/\.css$/,
+                use: [
+                    { loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
         ]
     }
 };
